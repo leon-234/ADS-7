d5e5f82c1ff4236c21f6a3767fbdc74aa6b743f9
leon-234 <rust824@mail.ru>
2026-05-23 22:11:00 +0300

    Ильин. 25-ИСТ-1-2

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,44 @@
 // Copyright 2022 NNTU-CS
 #include <iostream>
+#include <fstream>
+#include <random>
+
 #include "train.h"
 
+void experiment(const std::string& filename, int maxN, int mode) {
+    std::ofstream fout(filename);
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dist(0, 1);
+    for (int n = 2; n <= maxN; n += 2) {
+        Train train;
+        for (int i = 0; i < n; ++i) {
+            bool light = false;
+            switch (mode) {
+            case 0:
+                light = false;
+                break;
+            case 1:
+                light = true;
+                break;
+            case 2:
+                light = dist(gen);
+                break;
+            }
+            train.addCar(light);
+        }
+        train.getLength();
+        fout << n << "," << train.getOpCount() << std::endl;
+        std::cout << "n = " << n << " operations = " << train.getOpCount() << std::endl;
+    }
+    fout.close();
+}
+
 int main() {
-  Train train;
-  int count = 60; // кол-во вагонов
-
-  while (count--)
-    train.addCar(false);
-
-  std::cout << train.getLength() << std::endl;
-  std::cout << train.getOpCount() << std::endl;
-  return 0;
+    int maxN = 200;
+    experiment("off.csv", maxN, 0);
+    experiment("on.csv", maxN, 1);
+    experiment("random.csv", maxN, 2);
+}
+    return 0;
 }
